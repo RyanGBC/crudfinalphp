f962e511a2f9e9f9e4d4995318d3996bfcf03683
Ryan Gabriel Costa <ryangabrj@gmail.com>
2024-05-17 18:07:47 -0300

    adicionando tela para poder alterar dados do cliente

---
+++ telaalterarcliente.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alterando Cliente</title>
+</head>
+<body>
+<h1>Alterando Dados do cliente</h1>
+    <section id="principal2">
+        <h2>Editar Cliente </h2> 
+    <form action=".php" method="POST">
+         Nome:
+         <input type="text" name="cxnome" id="cxnome"/><br/><br/>
+         E-mail:
+         <input type="text" name="cxemail"/><br/><br/>
+         <input type="submit" value="ALTERAR">
+     </form><br/>
+    </nav>
+    <figure>
+        <a href="index.php" class="cxvoltar">
+        <img src="img/btnvoltarcli.png" alt="botao de voltar" id="btnvolt">
+        </a>
+    </figure>
+</body>
+</html>
